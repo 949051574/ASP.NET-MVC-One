59104c5a4dbd1261648a522d34b72cd9616619ae
zhouxiang <949051574@qq.com>
2019-06-04 11:10:00 +0800

    YunTiKu

---
+++ YunTiKu/YunTiKuWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YunTiKuWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
